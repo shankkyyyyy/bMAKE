49e46eaf2c190b51cbd5bb19f561d6da39ba667a
nine4four <nihal69845@gmail.com>
2026-03-20 01:10:46 +0530

    second version of cc

--- main.c
+++ main.c
@@ -48,6 +48,40 @@ int main(int argc,char** argv)
 
     // parsing the output name;
     char *CompiledProgramName = strtok(ProgramName,delim);
+    //allocating buffer through malloc 
+    char *buffer = malloc(100*4);
+    
+    printf("[#] Compiling %s Using GCC [#]",ProgramName);
+    // copying the gcc string into buffer after allocating memory 
+    strcpy(buffer,"gcc ");
+    
+    // catenates argv[i] to buffer with how many flags it has !! 
+
+    for (int i = 2;i<argc;i++)
+    {
+      if(strcmp(argv[i],"-w")==0)
+      {
+        strcat(buffer,"-Wall -Wextra ");
+      }
+      strcat(buffer,argv[i]);
+      strcat(buffer," ");
+    }
+    
+    // catenates hardcoded 
+    strcat(buffer,ProgramName);
+    strcat(buffer," -o ");
+    strcat(buffer,CompiledProgramName);
+    
+    //executing buffer to compile the code 
+    system(buffer);
+
+    // printing the result of executing buffer 
+    
+    printf("[#] Compiled %s Into %s [#]",)
+    // freeing buffer->compiling time needed buffer !
+    free(buffer);
+
+    return 0;
 
   }
 
