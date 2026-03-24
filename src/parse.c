d3e633b3b9ff59bd8ad310cd9b8069e954f49e29
nine4four <nihal69845@gmail.com>
2026-03-24 16:48:16 +0530

    parse.c partially has been completed and it is in the last stage of development, please check the comments and also add comments to the main while loop function

--- src/parse.c
+++ src/parse.c
@@ -1,85 +1,119 @@
+//
+// made by shankky !! 
+
+// include's 
 #include <stdio.h>
+// for memory allocation
 #include <stdlib.h> 
+// for string cutting,string manipulation's.
 #include <string.h>
+// for bool operations 
 #include <stdbool.h>
 
-// filename 
-// nameofthefile
-//
-// args
-// wextra
-// blah
-// end;
-// output
-// output program name 
-// endhl;
+// this is a simple project for simple compiling of simple .c file's 
+// this can be called a knockoff of CMake or Cninja 
+// this will have several bugs and memory problems !! if you find one please let me know 
+
+// defuault buffer size.
+#define BUFFER_d 1024
+
+// parsing function -> returns the system command for compiling the code
+// this is read from the .CCmake file 
+// the .CCmake file does not need to be in a particular order 
 
 char* parse(char* filename)
 {
+  // declaration of variables related to parsing; line 43 to see the use's of this 
+  char *args = malloc(BUFFER_d);
+  char *buffer = malloc(BUFFER_d);
+  char *Filename = malloc(BUFFER_d);
+  char *OutputName = malloc(BUFFER_d);
+
+  // this opens a file discriptor for opeing the file 
   FILE *fp = fopen(filename,"r");
+  // fp returns NULL if the fopen has failed. see docs on this. 
   if (fp==NULL)
   {
+    // strerr.
     printf("File can't be opened. \n");
+    // 'failed' is the char* for return 1;
     return "failed";
   }
-  char* outputName = malloc( 10 * 4);
-  char* args = malloc(10 * 4);
-  char* buffer = malloc(10 * 4);
-  char* Filename = malloc(10 * 8);
-  int i = 0;
-  bool FirstTimeArg = true;
-  while(fgets(buffer,sizeof(buffer),fp)!=NULL)
+
+  // i is the increment for the while loop; BAD PRACTICE.
+  int i = 1;
+  // loop for getting the first line or the contination of lines in the file; to know more read man fgets; 
+  while(fgets(buffer,BUFFER_d,fp)!=NULL)
   {
-    printf("reading the data ");
-    printf("%s\n\n",buffer);
-    if (strcmp("filename",buffer)==0)
+    
+    printf(" %d: %s",i,buffer);
+    strtok(buffer,"\n");
+    printf("%s",buffer);
+    i++;
+    if(strcmp(buffer,"filename")==0)
     {
-      fgets(Filename,sizeof(Filename),fp);
-      printf("%s\n\n",Filename);
-      i++;  
-      continue;
-    }
-    else if(i==1)
-    {
-      // right now buffer has 'args'
-      while(fgets(buffer,sizeof(buffer),fp)!=NULL && strcmp(buffer,"endhl")!=0)
+      if(fgets(Filename,sizeof(Filename),fp)!=NULL)
       {
-        printf("second while loop :%s",buffer);
-        if (FirstTimeArg)
+        printf("%s",Filename);
+        continue;
+      }
+      else 
+      {
+        printf("Compiling error at %d \n There is no filename present or NUllable character.\n",i);
+        return "failed";
+      }
+    }
+    else if (strcmp(buffer,"args")==0)
+    {
+      int j = 1;
+      while (fgets(buffer,BUFFER_d,fp)!=NULL && strtok(buffer,"\n"))
+      {
+        
+        if (j==1)
         {
           strcpy(args,buffer);
-          FirstTimeArg = false;
-          continue;
+          printf("string worked. \n");
+        }
+        
+        else 
+        {
+         strcat(args," ");
+         strcat(args,buffer);
+         printf("worked. \n");
         }
 
-        strcat(args,buffer);
-        printf("%s",args);
+        printf("%d: %s",j,args);
+        j++;
+        printf("%d",j);
 
+        if (strcmp(buffer,"output")==0)
+
+        {
+          
+          if(fgets(outputname,buffer_d,fp)!=null) 
+          {
+          
+            printf("all got !!\n worked perfectly.");
+          
+          }
+          
+         else   
+         {
+            printf("fgets returned null; ");
+            return "failed";
+          
+         }
+        }
       }
-      i++;
-    }
-    else if (i==2)
-    {
-      if(strcmp(buffer,"output")==0)
-      {
-        fgets(outputName,sizeof(outputName),fp);
-        printf("%s",outputName);
-        i++;
-      }
-    }
 
-    else if (i==3)
-    {
-      printf("%s \n %s \n %s.\n",outputName,args,Filename);
     }
-
-
+    
   }
-   printf("%s \n %s \n %s.\n",outputName,args,Filename);
 
+  printf("\n\n %s \n %s \n %s \n Everything data has been collected. \n\n",Filename,args,OutputName);
+  free(buffer);
   free(args);
-  free(outputName);
-  free(Filename);
+
   return "passed";
 }
 
